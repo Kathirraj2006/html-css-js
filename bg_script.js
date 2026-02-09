e47761a9c76ad262beb151841eebb630f5b841f2
Kathirraj <125266972+Kathirraj2006@users.noreply.github.com>
2026-02-09 23:49:23 +0530

    Delete bg_script.js

--- bg_script.js
+++
@@ -1,5 +0,0 @@
-let container=document.querySelector('#container');
-    const colors=['yellow','green','blue','black','white','gray','red','brown','lightblue'];
-function hit(){
-    container.style.backgroundColor=colors[Math.floor(Math.random()*colors.length)];
-}
